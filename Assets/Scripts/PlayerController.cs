e1abb14cb5981703e462ea9ead2cb69beb15782a
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-12-18 00:19:56 +0100

    update UI in the game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody2D rb2d;
+    private int count;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -25,6 +34,17 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count == 7)
+        {
+            winText.text = "You win!";
         }
     }
 }
